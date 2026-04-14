8567b87f6a6e11bb2ebecab65afc86b13d41b897
bonface123 <youremail@example.com>
2026-04-14 11:43:40 +0300

    edit projects

--- update_hero.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content index.html
-$content = $content -replace 'src="assets/trizah.png"', 'src="assets/AboutTrizah.png"'
-$content = $content -replace 'alt="Trizah Nasumba - Digital Creative"', 'alt="Trizah Nasumba - About Me"'
-Set-Content index.html $content
